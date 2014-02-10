12e451aef4b9aa4b6fb6a4d575a30470ea237569
Alexander Saye <alexandersaye@live.co.uk>
2014-02-10 01:10:04 +0000

    Player Movement
    Players can now move with the mouse/keyboard and gamepad.
    Keyboard/Mouse controls need some work done on them eventually, as they
    are very odd to use.
    Camera fixes have been implemented so there is no stuttering.

--- OneNineOneFour/Assets/Players/scrPlayer.cs
+++ OneNineOneFour/Assets/Players/scrPlayer.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class scrPlayer : MonoBehaviour
 {
 	private static bool switching = false;	// Prevents constant switching between characters.
+
+	public float WalkSpeed;	// The speed to apply when walking.
+	public float JumpSpeed;	// The speed to apply when jump is pressed.
+	private bool jumping = false;	// Prevents constant jumping.
 	
 	public bool CanControl = false;	// Whether this specific player can be controlled.
+	public bool Waiting = false;	// Whether this specific player has been signalled to wait.
 	private GameObject sibling;	// The other sibling.
 
 	// Use this for initialization
 	void Start ()
 	{
+		Application.targetFrameRate = 60;
+
 		// Disable the renderer when not in the editor.
 		renderer.enabled = false;
 
@@ -28,32 +35,115 @@ public class scrPlayer : MonoBehaviour
 	{
 		if (CanControl == true)
 		{
+			HandleSwitch();
+		}
+	}
 
-			if (Input.GetAxis ("Switch") > 0)
-			{
-				// This prevents constant switching between players while the Switch axis is down.
-				if (switching == false)
-				{
-					CanControl = false;	// Stop manual control.
-					sibling.GetComponent<scrPlayer>().CanControl = true;	// Pass manual control to the other sibling.
-					Camera.main.GetComponent<scrCamera>().Target = sibling;	// Target the camera to the other sibling.
-					switching = true;	// Switching is taking place.
-
-					Debug.Log ("Switched to " + sibling.name);
-				}
-			}
-			else
-			{
-				switching = false;	// Switching is no longer happening.
-			}
-
-
-
-
+	void FixedUpdate()
+	{
+		if (CanControl)
+		{
+			HandleJump();
+			HandleMove();
 		}
 		else
 		{
+			HandleFollow();
+		}
+	}
 
+	void HandleSwitch()
+	{
+		if (Input.GetAxis ("Switch") != 0)
+		{
+			// This prevents constant switching between players while the Switch axis is down.
+			if (switching == false)
+			{
+				// Stop manual control.
+				CanControl = false;
+
+				// Pass manual control to the other sibling.
+				sibling.GetComponent<scrPlayer>().CanControl = true;
+				
+				// Target the camera to the other sibling.
+				Camera.main.GetComponent<scrCamera>().ChangeTarget(sibling);
+
+				switching = true;
+			}
+		}
+		else
+		{
+			// Switch is no longer held so allow it to be activated again on the next press.
+			switching = false;
+		}
+	}
+
+	void HandleJump ()
+	{
+		if (Input.GetAxis ("Jump") != 0)
+		{
+			if (jumping == false)
+			{
+				// Create a ray pointing down.
+				Ray floorCheck = new Ray(this.transform.position, Vector3.down);
+
+				// Check if the player is on (slightly above for more lenient input) the floor.
+				if (Physics.Raycast(floorCheck, (this.transform.localScale.y + 0.1f)))
+					this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, JumpSpeed, this.rigidbody.velocity.z);
+
+
+				jumping = true;
+			}
+		}
+		else
+		{
+			// Jumping is no longer held so allow it to be activated again on the next press.
+			jumping = false;
+		}
+	}
+
+	void HandleMove ()
+	{
+		Vector3 moveDirection = Vector3.zero;
+
+		// Forwards/Backwards
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			// Get the camera's forward vector.
+			Vector3 forward = Camera.main.transform.forward;
+			forward.y = 0;	// Remove the vertical value to only use horizontal components.
+		
+			// Add this direction to the move direction, with its sign depending on the sign of the vertical input.
+			moveDirection += forward * Input.GetAxis("Vertical");
+		}
+
+		// Strafing
+		if (Input.GetAxis("Horizontal") != 0)
+		{
+			// Get the camera's right vector.
+			Vector3 right = Camera.main.transform.right;
+			right.y = 0;	// Remove the vertical value to only use horizontal components.
+			
+			// Add this direction to the move direction, with its sign depending on the sign of the vertical input.
+			moveDirection += right * Input.GetAxis("Horizontal");
+		}
+
+		// Face the desired direction.
+		this.transform.LookAt(this.transform.position + moveDirection);
+
+		// Set the horizontal components of the velocity to the move direction's horizontal components.
+		this.rigidbody.velocity = new Vector3(moveDirection.x * WalkSpeed, this.rigidbody.velocity.y, moveDirection.z * WalkSpeed);
+	}
+
+	void HandleFollow ()
+	{
+		if (Waiting == false)
+		{
+			this.rigidbody.velocity = new Vector3(0, this.rigidbody.velocity.y, 0);
+		}
+		else
+		{
+			this.rigidbody.velocity = new Vector3(0, this.rigidbody.velocity.y, 0);
 		}
 	}
 }
--- OneNineOneFour/Assets/scrCamera.cs
+++ OneNineOneFour/Assets/scrCamera.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class scrCamera : MonoBehaviour
 {
 	public GameObject Target;	// The target object to follow.
+	private bool changingTarget = false;
 	public float Distance = 3;
 	private float pitch = 0;
 	private float yaw = Mathf.PI;
@@ -12,13 +13,23 @@ public class scrCamera : MonoBehaviour
 	public Vector2 MouseSensitivity = new Vector2(0.5f, 0.5f);	// The sensitivity of the mouse input to move the camera.
 	
 	//public bool Tracking;	// Whether to track mouse motion (off for menus for example).
-	
+
+	public void ChangeTarget(GameObject target)
+	{
+		Target = target;
+		changingTarget = true;
+	}
+
 	void Start ()
 	{
-	
+		if (Target != null)
+		{
+			this.transform.position = Target.transform.position + new Vector3(Mathf.Sin (yaw), Mathf.Sin (pitch), Mathf.Cos(yaw)) * Distance;
+			this.transform.LookAt(Target.transform.position);
+		}
 	}
 	
-	void Update ()
+	void FixedUpdate ()
 	{		
 		if (Target != null)
 		{
@@ -26,64 +37,65 @@ public class scrCamera : MonoBehaviour
 			
 			// Cast a ray from the target to the camera and check for the first surface hit by the ray.
 			Ray wallClipPreventer = new Ray(Target.transform.position, -direction);
-			RaycastHit wallClip;
+			RaycastHit hit;
 			float correctDistance;
-			if (Physics.Raycast(wallClipPreventer, out wallClip, Distance))
-				correctDistance = wallClip.distance - 0.2f; // Set the correct distance of the camera, preventing wall clipping by pushing it closer than its desired distance, plus 0.2 units so it is not inside the wall.
+			if (Physics.Raycast(wallClipPreventer, out hit, Distance))
+				correctDistance = hit.distance - 0.2f; // Set the correct distance of the camera, preventing wall clipping by pushing it closer than its desired distance, plus 0.2 units so it is not inside the wall.
 			else
 				correctDistance = Distance;	// No clipping is observed so use the desired orbit distance.
-			
-			//Debug.DrawLine(wallClipPreventer.origin, wallClipPreventer.origin + wallClipPreventer.direction * Distance);
-			
-			// Smoothly translate the camera to its target position.
-			this.transform.position = Vector3.Lerp(this.transform.position, Target.transform.position + new Vector3(Mathf.Sin (yaw), Mathf.Sin (pitch), Mathf.Cos(yaw)) * correctDistance, TranslationSmoothing);
-			
-			// Smoothly rotate the camera to face its target object.
-			this.transform.rotation = Quaternion.Lerp (this.transform.rotation, Quaternion.LookRotation(direction), RotationSmoothing);
-		}
-		else
-		{
-			
-		}
-		
-		// Change the distance and limit to min and max values.
-		Distance -= Input.GetAxis("Mouse ScrollWheel");
-		if (Distance > 10)
-			Distance = 10;
-		else if (Distance < 2)
-			Distance = 2;
 
-		if (Input.GetAxis("Move Camera") > 0)
-		{
-			// Change the pitch and limit to +- Pi / 2 radians.
-			pitch += Input.GetAxis("Mouse Y") * MouseSensitivity.y;
-			if (pitch > Mathf.PI / 2)
-				pitch = Mathf.PI / 2;
-			else if (pitch < -Mathf.PI / 2)
-				pitch = -Mathf.PI / 2;
-			
-			// Change the yaw and wrap around 2 Pi radians.
-			yaw -= Input.GetAxis("Mouse X") * MouseSensitivity.x;
-			if (yaw > 2 * Mathf.PI)
-				yaw = 0;
-			else if (yaw < 0)
-				yaw = 2 * Mathf.PI;
-		}
-		else
-		{
-			// Change the pitch and limit to +- Pi / 2 radians.
-			pitch += Input.GetAxis("Pitch Camera") * MouseSensitivity.y * 0.3f;
-			if (pitch > Mathf.PI / 2)
-				pitch = Mathf.PI / 2;
-			else if (pitch < -Mathf.PI / 2)
-				pitch = -Mathf.PI / 2;
-			
-			// Change the yaw and wrap around 2 Pi radians.
-			yaw -= Input.GetAxis("Yaw Camera") * MouseSensitivity.x * 0.3f;
-			if (yaw > 2 * Mathf.PI)
-				yaw = 0;
-			else if (yaw < 0)
-				yaw = 2 * Mathf.PI;
+			// Find the target position in orbit around the target.
+			Vector3 desiredPosition = Target.transform.position + new Vector3(Mathf.Sin (yaw), Mathf.Sin (pitch), Mathf.Cos(yaw)) * correctDistance;
+
+			// Smoothly translate the camera to its target position.
+			this.transform.position = Vector3.Lerp(this.transform.position, desiredPosition, TranslationSmoothing);
+
+			// Only allow rotation when not travelling to a new target.
+			if (changingTarget == true)
+		    {
+				if (Vector3.Distance(this.transform.position, desiredPosition) < 0.1f)
+					changingTarget = false;
+			}
+			else
+			{
+				// Change the distance and limit to min and max values. Get from both mouse and gamepad????.
+				Distance -= Input.GetAxis("Mouse ScrollWheel");
+				if (Distance > 10)
+					Distance = 10;
+				else if (Distance < 2)
+					Distance = 2;
+
+				// Get input from both mouse and gamepad.
+				if (Input.GetAxis("Move Camera") != 0)
+				{
+					// Change the pitch and limit to +- Pi / 2 radians.
+					pitch += Input.GetAxis("Mouse Y") * MouseSensitivity.y;
+					
+					// Change the yaw and wrap around 2 Pi radians.
+					yaw += Input.GetAxis("Mouse X") * MouseSensitivity.x;
+				}
+				else
+				{
+					// Change the pitch and limit to +- Pi / 2 radians.
+					pitch += Input.GetAxis("Pitch Camera") * MouseSensitivity.y * 0.2f;
+					
+					// Change the yaw and wrap around 2 Pi radians.
+					yaw += Input.GetAxis("Yaw Camera") * MouseSensitivity.x * 0.3f;
+				}
+
+				if (pitch > Mathf.PI / 2)
+					pitch = Mathf.PI / 2;
+				else if (pitch < -Mathf.PI / 2)
+					pitch = -Mathf.PI / 2;
+
+				if (yaw > 2 * Mathf.PI)
+					yaw = 0;
+				else if (yaw < 0)
+					yaw = 2 * Mathf.PI;
+
+				// Smoothly rotate the camera to face its target object.
+				this.transform.rotation = Quaternion.Lerp (this.transform.rotation, Quaternion.LookRotation(direction), RotationSmoothing);
+			}
 		}
 	}
 }
